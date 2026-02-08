3fb139d143b99f4d3625cbb3940660d8e76fa5d2
argho001 <debangshusaha970@gmail.com>
2026-02-09 04:34:49 +0600

    Add TURN servers for cross-network calls

--- client/main.js
+++ client/main.js
@@ -138,6 +138,16 @@ function setupPeerConnection() {
       { urls: 'stun:stun2.l.google.com:19302' },
       { urls: 'stun:stun3.l.google.com:19302' },
       { urls: 'stun:stun4.l.google.com:19302' },
+      {
+        urls: 'turn:openrelay.metered.ca:80',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      }
     ],
   });
 
